7cbe4b623e62f158362bf18b1f77cc9dc663bc67
Soumya Sinha <soumya.sinha0011@gmail.com>
2026-06-20 20:07:45 +0530

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Card from './Components/Card'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <Navbar />
+      <div>
+        <div className="container" style={{ width: '600px', height: '200px', backgroundColor: 'white', borderRadius: '30px', marginTop: '40px' }}>
+          <div>
+            <h4 style={{ textIndent: '15px', paddingTop: '20px' }}>Add Notes here</h4>
+          </div>
+          <div className="container-fluid">
+            <form role="search">
+              <input className="form-control me-2" type="search" placeholder="Title goes here.." aria-label="Search" style={{ border: 'none', backgroundColor: '#22004256', height: '40px' }} />
+              <input className="form-control me-2" type="search" placeholder="Note goes here.." aria-label="Search" style={{ border: 'none', backgroundColor: '#22004256', height: '40px', marginTop: '3px' }} />
+              <button className="btn " style={{ color: "white", backgroundColor: '#220042', width: '140px', marginTop: '15px', marginLeft: '200px' }} type="submit" >Search</button>
+            </form>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
+      <Card/>
     </>
   )
 }
-
-export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar" style={{backgroundColor:'rgba(23, 0, 45, 0.76)'}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" style={{color:'rgb(255, 237, 215)'}} href="#">
+                            Note IT
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
